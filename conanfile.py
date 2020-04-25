e1b0cdcd179fba9d98d26aa08d1fec7a4cf1d4c7
offa <bm-dev@yandex.com>
2020-04-25 18:24:01 +0200

    Libcurl update.

--- conanfile.py
+++ conanfile.py
@@ -23,7 +23,7 @@ class RestclientcppConan(ConanFile):
         "revision": version
     }
     requires = (
-        "libcurl/7.66.0@bincrafters/stable",
+        "libcurl/7.69.1@bincrafters/stable",
         "jsoncpp/1.9.2"
     )
 
